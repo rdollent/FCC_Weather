8f693ef8faa6bb88f6a6a186ba15fe4628197f66
rdollent <rumbalumba89@gmail.com>
2017-03-01 11:12:27 -0700

    Update Weather_JS.js
    -changed API from Darksky (which uses proxy) to OpenWeather.
    -lacking in icons. Will update at a later date.

--- Weather_JS.js
+++ Weather_JS.js
@@ -7,10 +7,18 @@ if ("geolocation" in navigator)  {
 };
 */
 
-$(document).ready(function() {
+//https://openweathermap.org/api
+//new API key
+//6b548bc27eb953d1c156738e470bcb8b
+//http://api.openweathermap.org/data/2.5/weather?late=&lon=&APPID=6b548bc27eb953d1c156738e470bcb8b
+//http://api.openweathermap.org/data/2.5/weather?q=London&APPID=6b548bc27eb953d1c156738e470bcb8b
+
+window.onload = runThis;
+
+function runThis() {
 
   //latitude,longitude, fahrenheit, celsius, weather data JSON, weather icon
-  var lat, lon, f, c, weatherData, icon;
+  let lat, lon, f, c, weatherData, icon;
 
   //function to get latitude and longitude for use with ajax
   function getLocation() {
@@ -21,67 +29,66 @@ $(document).ready(function() {
         getData(); //use lat,lon to make API call if geolocation is supported
       });
     } else {
-      var y = document.getElementByClass("city");
-      y.innerHTML = "Geolocation is not supported";
+      let y = document.getElementsByClassName("city");
+      y[0].innerHTML = "Geolocation is not supported";
+      
     }
   }
-
   getLocation();
-
+  
   //ajax/API call
   function getData() {
-    $.ajax({
-      type: "GET",
-      url: "https://crossorigin.me/https://api.darksky.net/forecast/ed9ad4aa458eb9fbf9a18beb3071a399/" + lat + "," + lon,
-      success: function(a) {
-        //pass JSON data as "a" onto variable
-        weatherData = a;
+    let xhttp = new XMLHttpRequest();
+    let endpoint = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=57c7e2a35b6157e4df790570f975a6f7&units=imperial`;
+    xhttp.open("GET", endpoint, true);
+    xhttp.onload = function() {
+        weatherData = JSON.parse(xhttp.responseText);
         console.log(weatherData); //check what JSON looks like
-        changeData(); //if success, run this function
-      }
-    });
+        changeData(weatherData); //if success, run this function
+    };
+    xhttp.send();
   }
 
   //convert F to C
-  function tempConvert(a) {
-    return Math.round(((a - 32) * 5) / 9);
+  function tempConvert(x) {
+    return Math.round(((x - 32) * 5) / 9);
   }
 
   //change HTML content using JSON data
-  function changeData() {
-    var strCity = weatherData.timezone; //put "Country/City" string into variable
-    $(".city").html(strCity.substring(strCity.search("/") + 1, strCity.length)); //extract city name from string
-    f = weatherData.currently.temperature;
+  function changeData(w) {
+    //var strCity = w.name; //put "Country/City" string into variable
+    $(".city").html(w.name); //extract city name from string
+    f = w.main.temp;
     c = tempConvert(f); //convert fahrenheit to celsius
-    icon = weatherData.currently.icon;
-    console.log(icon);
+    icon = w.weather[0].description;
+    console.log(f);
     $(".temp").html(c);
     $(".unit").text("C").css({
       "color": "blue"
     });
     //add icons
-    var skycons = new Skycons({
+    /*var skycons = new Skycons({
       "color": "white"
     });
     skycons.add(document.getElementById("icon"), icon);
-    skycons.play();
+    skycons.play();*/
   }
 
   //click event to change C to F and temps or vice versa
   $(".unit").on("click", function() {
-$('.temp, .unit').fadeOut("fast")
-  .delay(100)
-  .queue(function(n) {
-    if (this.textContent === "C") {
-    $(this).html("F");
-  $(".temp").html(Math.round(f));
-    } else {
-      $(this).html("C");
-      $(".temp").html(c);
-    };
-  n();
-});
+    $('.temp, .unit').fadeOut("fast")
+    .delay(100)
+    .queue(function(n) {
+      if (this.textContent === "C") {
+        $(this).html("F");
+        $(".temp").html(Math.round(f));
+      } else {
+        $(this).html("C");
+        $(".temp").html(c);
+      }
+    n();
+  });
     $('.temp, .unit').fadeIn("fast");
   });
 
-});
+}
